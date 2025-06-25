fcc25754d94c124bed824f2a307dcba8583a6961
Nutrient <support@nutrient.io>
2025-06-25 20:47:56 +0200

    Nutrient iOS SDK Catalog example app

---
+++ Catalog/Examples/Security/PasswordPresetExample.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright © 2017-2025 PSPDFKit GmbH. All rights reserved.
+//
+//  The Nutrient sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+import PSPDFKit
+import PSPDFKitUI
+
+class PasswordPresetExample: Example {
+
+    override init() {
+        super.init()
+        title = "Password preset"
+        category = .security
+    }
+
+    override func invoke(with delegate: ExampleRunnerDelegate) -> UIViewController {
+        let document = AssetLoader.document(for: "Password-Protected.pdf")
+        document.unlock(withPassword: "test123")
+        return PDFViewController(document: document)
+    }
+}
